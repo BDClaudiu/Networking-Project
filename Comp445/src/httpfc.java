3dd86c48c96b01f27f7d55c6bd4f87ca8c5e0095
BDClaudiu <bacisor_Clau@yahoo.com>
2018-03-12 13:46:28 -0400

    File Transfer Using sockets in Java

---
+++ Comp445/src/httpfc.java
@@ -0,0 +1,298 @@
+/*
+ * COMP 445 
+ * Assignment 2
+ * Client Server
+ * 
+ * Bacisor Claudiu ID: 27735332
+ * Marzie Shafiee ID: 
+ * 
+*/
+
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class httpfc {
+	
+	private final static String SERVER_ADDRESS="localhost";
+	private static String method;
+	private static String fileName;
+	
+	//Buffer Reader instance variables
+	private static BufferedReader bufferedReader;
+	private static String buffer = null;
+	private static int nextChar = 0;  
+
+	//Checking the end of file
+	public static boolean EOF() throws IOException  
+	{ 
+		char fileEnd;
+
+		if (buffer == null || nextChar > buffer.length()) 
+		{
+			buffer = bufferedReader.readLine();	
+			nextChar = 0;
+		}
+	    if (buffer == null) 
+	    {
+		    fileEnd = (char)0xFFFF; 
+	    }
+	    else if (nextChar == buffer.length()) 
+	    {
+		    fileEnd = '\n';
+	    }
+	    else  
+	    {
+		    fileEnd = buffer.charAt(nextChar);
+	    }
+		return fileEnd == (char)0xFFFF;
+	}
+
+	//Method that reads a line using the character method
+	public static String line(BufferedReader inStream) throws IOException 
+	{
+	    StringBuffer stringBuffer = new StringBuffer(100);
+	    bufferedReader = inStream;
+	    
+	    //Reading a char until we reach a new line
+	    char c = readCharacter();
+	    
+	    while (c != '\n') 
+	    {
+	  	  stringBuffer.append(c);
+	      c = readCharacter();
+	    }
+	    
+	    return stringBuffer.toString();
+	}
+	//Method that reads a character
+	private static char readCharacter() throws IOException 
+	{  
+	      char character;
+	    
+	      if (buffer == null || nextChar > buffer.length()) 
+	      {
+				buffer = bufferedReader.readLine();
+				nextChar = 0;
+	      }         
+	      if (buffer == null) 
+	      {
+	  	    character = (char)0xFFFF; 
+	      }
+	      else if (nextChar == buffer.length()) {
+	  	    character = '\n';
+	      } 
+	      else  
+	      {
+	  	    character = buffer.charAt(nextChar);
+	      }
+		  if (buffer == null) 
+		  {
+		      System.out.println("Empty buffer!");;
+		  }
+	    
+	    nextChar++;
+	    return character;
+	}
+	
+public static void main(String[] args) throws Exception {
+	int server_port =8080;
+	String method="";
+	String file_name="";
+	String file_content="";
+	
+	System.out.println("**********Client is Running**********");	
+	ArrayList <String> clientRequestCommandLine = new ArrayList<String>(Arrays.asList(args));
+	
+	
+	
+/*
+ * -------------------------------------------------------------------------------------------------------
+ * POST and GET methods options  
+ * -------------------------------------------------------------------------------------------------------
+ */
+	
+	//I WANNNA CHANGE THIS...ITS STUPID!!! to have a if condition for GET and get....POST and post (i did not include Post)
+	
+	//GET request without file name
+	if(clientRequestCommandLine.contains("get") && clientRequestCommandLine.size()==1)
+	{
+		method="GET";
+		file_name="";	
+	}
+	else if(clientRequestCommandLine.contains("get") && clientRequestCommandLine.size()==1)
+	{
+		method="GET";
+		file_name="";	
+	}
+	
+	//GET request with file name
+	else if(clientRequestCommandLine.contains("GET") && clientRequestCommandLine.size()>1)
+	{
+		method="GET";
+		int position =clientRequestCommandLine.indexOf("GET");
+		file_name=clientRequestCommandLine.get(position+1);	
+	}
+	else if(clientRequestCommandLine.contains("get") && clientRequestCommandLine.size()>1)
+	{
+		method="GET";
+		int position =clientRequestCommandLine.indexOf("get");
+		file_name=clientRequestCommandLine.get(position+1);	
+	}
+
+	//POST request 
+	else if(clientRequestCommandLine.contains("POST"))
+	{
+		method="POST";
+		int position =clientRequestCommandLine.indexOf("POST");
+		if(clientRequestCommandLine.size()>2)
+		{
+			file_name=clientRequestCommandLine.get(position+1);
+			for(int i=position+2;i<clientRequestCommandLine.size();i++)
+			{
+				file_content+=clientRequestCommandLine.get(i);
+			}
+		}
+		else
+			System.out.println("Please input a file name followed by the file content");
+	}	
+	else if(clientRequestCommandLine.contains("post"))
+	{
+		method="POST";
+		
+		int position =clientRequestCommandLine.indexOf("post");
+		if(clientRequestCommandLine.size()>2)
+		{
+			file_name=clientRequestCommandLine.get(position+1);
+			for(int i=position+2;i<clientRequestCommandLine.size();i++)
+			{
+				file_content+=clientRequestCommandLine.get(i)+" ";
+			}
+		}
+		else
+			System.out.println("Please input a file name followed by the file content");	
+	}
+	else 
+	{
+		System.out.println("Invalid input.Valid inputs are: GET, GET filename, POST filename filecontent. ");
+		System.out.print("System shut down imminent!!");
+		System.exit(0);
+	}
+	
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * Opening connection to server
+ * Sending requests according to the above mentioned methods
+ * --------------------------------------------------------------------------------------------------------------------	
+ */
+	Socket clientSocket;
+	BufferedReader inStream=null;
+	PrintWriter outStream=null;
+	
+	String command;
+	
+	clientSocket= new Socket(SERVER_ADDRESS,server_port);
+	inStream= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	outStream= new PrintWriter(clientSocket.getOutputStream());
+	
+	// Clients requests
+	
+	//CASE 1 GET
+	if(method.equalsIgnoreCase("GET")&& file_name==null)
+	{
+		command=method;
+		outStream.println(command);
+		outStream.flush();
+		System.out.println("Sending request " + command +" to server");
+	}	
+	
+	
+	//CASE 2 GET filename
+	if(method.equalsIgnoreCase("GET")&& file_name!=null)
+	{
+		command=method+" "+file_name;
+		outStream.println(command);
+		outStream.flush();
+		System.out.println("Sending request " + command +" to server");
+	}	
+	
+	//CASE 3 POST filename content
+	if(method.equalsIgnoreCase("POST"))
+	{
+		command=method+" "+file_name+"/"+file_content;
+		outStream.println(command);
+		outStream.flush();
+		System.out.println("file content " + file_content);
+		System.out.println("Sending request " + command +" to server");
+		
+		File newFile = new File(file_name);
+		
+		if(!newFile.exists() || newFile.isDirectory())
+		{
+			outStream.println(file_content);
+		}
+		else
+		{
+			FileReader file_Reader = new FileReader(newFile);
+			BufferedReader buffered_Reader =  new BufferedReader (file_Reader);
+			
+			while(buffered_Reader.readLine() !=null)
+			{
+				outStream.println(buffered_Reader.readLine());	
+			}
+			file_Reader.close();
+			outStream.flush();
+			outStream.close();
+		}
+	}
+	
+	if(outStream.checkError())
+	{
+		clientSocket.close();
+		throw new Exception("Transfer incomplete.");
+	}
+	
+	//Server Response
+	
+	//CASE 1 GET 
+	if(method.equalsIgnoreCase("GET") && file_name=="")
+	{
+		System.out.println("File list: ");
+		Scanner scan= new Scanner(inStream).useDelimiter("\\A");
+		String result=scan.hasNext() ? scan.next(): "";
+		System.out.println(result);
+		scan.close();
+	}
+	
+	//CASE 2 GET filename  
+	if(method.equalsIgnoreCase("GET") && file_name!="")
+	{
+		String msg = line(inStream);
+		if(!msg.equalsIgnoreCase("success"))
+		{
+			System.out.println("ERROR 404. FILE NOT FOUND");
+			clientSocket.close();
+			return;
+		}
+		
+		while(!EOF())
+		{
+			String line=line(inStream);
+			File file = new File(file_name);
+			FileWriter fileWriter = new FileWriter (file,true);
+			PrintWriter out= new PrintWriter (fileWriter);
+			System.out.println(line);
+			out.close();	
+		}
+	}
+		
+	//CASE 3 POST
+	if(method.equalsIgnoreCase("POST"))
+	{	
+		System.out.println("File has been succesfully send");
+	}
+	clientSocket.close();
+	
+	}
+}
---
+++ Comp445/src/httpfs.java
@@ -0,0 +1,270 @@
+/*
+ * COMP 445 
+ * Assignment 2
+ * Server Server
+ * 
+ * Bacisor Claudiu ID: 27735332
+ * Marzie Shafiee ID: 
+ * 
+*/
+
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class httpfs {
+
+private File fileDir;
+private Socket clientSocket;
+private ServerSocket serverSocket;
+private int port;
+private ArrayList<String> userCommandLine;
+
+//Buffer Reader instance variables
+private static BufferedReader bufferedReader;
+private static String buffer = null;
+private static int nextChar = 0;  
+
+//Checking the end of file
+public static boolean EOF() throws IOException  { 
+	char fileEnd;
+
+	if (buffer == null || nextChar > buffer.length()) 
+	{
+		buffer = bufferedReader.readLine();	
+		nextChar = 0;
+	}
+    if (buffer == null) 
+    {
+	    fileEnd = (char)0xFFFF; 
+    }
+    else if (nextChar == buffer.length()) 
+    {
+	    fileEnd = '\n';
+    }
+    else  
+    {
+	    fileEnd = buffer.charAt(nextChar);
+    }
+	return fileEnd == (char)0xFFFF;
+}
+
+//Method that reads a line using the character method
+public static String line(BufferedReader inStream) throws IOException 
+{
+    StringBuffer stringBuffer = new StringBuffer(100);
+    bufferedReader = inStream;
+    
+    //Reading a char until we reach a new line
+    char c = readCharacter();
+    
+    while (c != '\n') 
+    {
+  	  stringBuffer.append(c);
+      c = readCharacter();
+    }
+    
+    return stringBuffer.toString();
+}
+//Method that reads a character
+private static char readCharacter() throws IOException {  
+    char character;
+    
+      if (buffer == null || nextChar > buffer.length()) 
+      {
+			buffer = bufferedReader.readLine();
+			nextChar = 0;
+      }         
+      if (buffer == null) 
+      {
+  	    character = (char)0xFFFF; 
+      }
+      else if (nextChar == buffer.length()) {
+  	    character = '\n';
+      } 
+      else  
+      {
+  	    character = buffer.charAt(nextChar);
+      }
+	  if (buffer == null) 
+	  {
+	      System.out.println("Empty buffer!");;
+	  }
+    
+    nextChar++;
+    return character;
+}
+	public httpfs(String[] args) throws Exception {
+		
+		// Default port number 
+		port=8080;
+		
+		// Creates a new File instance by converting the given pathname string into an abstract path name
+		fileDir= new File("C:\\Core\\WorkBenches\\Java WorkBench\\Comp445\\StorageDirectory");
+		
+		//Here we are storing the command line commands inputs from user into a ArrayList of Strings (same as in assignment1)
+		userCommandLine = new ArrayList<String>(Arrays.asList(args));
+				
+	/* 3 options: 
+	 * -v : Prints debugging messages
+	 * -p : Specifies a new port number that the server will listen and serve at. 
+	 * Default: 8080
+	 * -d : Specifies a new directory that the server will use to read/write requested files. 
+	 * Default: C:\\Core\\WorkBenches\\Java WorkBench\\Comp445\\StorageDirectory
+	 */
+		
+	if(userCommandLine.contains("-v"))
+	{
+		System.out.println("Prints debugging messages");	
+	}
+		
+	if(userCommandLine.contains("-p"))
+	{	// Setting the port number with the value preceding the command line with the index "-p";
+		// Since value is a string, we need to convert it into integer
+		System.out.println("Specifies the port number that the server will listen and server at.\n Default: 8080\n New: "+ Integer.parseInt(userCommandLine.get(userCommandLine.indexOf("-p")+1)));	
+		port=Integer.parseInt(userCommandLine.get(userCommandLine.indexOf("-p")+1));
+	}
+		
+	if(userCommandLine.contains("-d"))
+	{// Setting the file directory with the string value preceding the command line with the index "-d";
+		System.out.println("Specifies the directory that the server will use to read/write requested files.\n Default:C:\\Core\\WorkBenches\\Java WorkBench\\Comp445\\StorageDirectory\n New: "+ userCommandLine.get(userCommandLine.indexOf("-d")+1));
+		fileDir= new File (userCommandLine.get(userCommandLine.indexOf("-d")+1));
+	}
+		
+		
+}
+	
+	private void setConnection() throws Exception {
+		
+	serverSocket = new ServerSocket(port);	
+	System.out.println("*******Server is Running********");
+	
+	// While true implies it is always the case, as such, the server will always be listening and accepting clients (will not terminate after lets say, a request/of after a clients connects)
+	while (true)
+	{
+		clientSocket=serverSocket.accept();
+		System.out.println("A client connected");
+		
+		String userReq="";
+		String fileName="";
+		
+		BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		PrintWriter outStream= new PrintWriter(clientSocket.getOutputStream());
+		
+		userReq= line(inStream);
+		userReq=userReq.toLowerCase();
+		
+		System.out.println("Client Request : " + userReq);
+		
+		
+		//CASE 1 (GET)
+		if (userReq.startsWith("get")) 
+		{
+			fileName=userReq.substring(3).trim();
+			
+			// CASE 1.1 GET 	
+			// If the command is GET, we loop through the file directory and display the files
+			if(fileName.equals("")) 
+			{
+					String[] files = fileDir.list();
+					for (int i = 0; i < files.length; i++)
+					{
+						outStream.println(files[i]);
+					}
+					outStream.flush();
+					outStream.close();
+
+				if (outStream.checkError())
+				{
+					throw new Exception("Error. File Display Incomplete...");
+				}
+			} //end if statement
+			
+			// CASE 1.2 GET with file name
+			else 
+			{
+				//creating a new file that has the same directory and command line filename
+				File file = new File(fileDir, fileName);
+
+				if ( (!file.exists()) || file.isDirectory() )
+				{
+					outStream.println("ERROR 404: File Not Found!");
+				}
+
+				else
+				{
+					outStream.println("Success");
+					FileReader fileReader = new FileReader(file);
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+					String fileLine;
+					while((fileLine = bufferedReader.readLine()) != null)
+					{
+						outStream.println(fileLine);
+					}
+					fileReader.close();
+
+					outStream.flush();
+					outStream.close();
+				}
+
+				if (outStream.checkError())
+				{
+					throw new Exception();
+				}
+
+			}//end of else
+		} //end of if GET (CASE 1)
+		 
+		//CASE 2 POST
+		if (userReq.startsWith("post")) 
+		{	// Getting the index of  "/"
+			int delimiterPosition = userReq.indexOf("/");
+			//Removing everything that comes before "/"
+			fileName = userReq.substring(4, delimiterPosition).trim();
+
+			File file = new File(fileDir, fileName);
+
+			if (file.exists())
+			{
+				PrintWriter writer = new PrintWriter(file);
+				writer.print("");
+				writer.close();
+			}
+
+			while (EOF() == false)
+			{
+				String line = line(inStream);
+				FileWriter fileWriter = new FileWriter(file, true);
+				PrintWriter printWriter = new PrintWriter(fileWriter);
+
+				printWriter.print(line);
+				printWriter.close();
+
+			}
+
+			outStream.println("success");
+			outStream.flush();
+			outStream.close();
+
+			if (outStream.checkError())
+			{
+				throw new Exception();
+			}
+
+		}//end of if POST loop
+		
+	}//end of while loop
+}
+	
+
+//Main Method	
+public static void main(String[] args) throws Exception {
+		
+	httpfs server = new httpfs(args);
+
+	server.setConnection();
+
+	}
+}
